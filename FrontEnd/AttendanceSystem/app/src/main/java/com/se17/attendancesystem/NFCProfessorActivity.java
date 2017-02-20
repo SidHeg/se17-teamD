37746b62e4c2abf9d60bd82451d081ce7349b8ea
saravanan19 <bsaran19@gmail.com>
2017-02-20 00:43:25 -0500

    NFC Reader bug fix

--- FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/NFCProfessorActivity.java
+++ FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/NFCProfessorActivity.java
@@ -1,10 +1,15 @@
 package com.se17.attendancesystem;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /*
     Activity that helps professor to write payload in the NFC Tags
@@ -12,8 +17,10 @@ import android.widget.EditText;
 
 public class NFCProfessorActivity extends AppCompatActivity {
 
+    private Tag nfcTag;
     private EditText nfcText;
     private Button btnNfcWrite;
+    private NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +28,38 @@ public class NFCProfessorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nfcprofessor);
         nfcText = (EditText) findViewById(R.id.nfctext);
         btnNfcWrite = (Button) findViewById(R.id.btnNFCWrite);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
     }
 
+    public void onPause() {
+        super.onPause();
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+    
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Toast.makeText(getApplicationContext(),"NFC Tag detected",Toast.LENGTH_SHORT).show();
+    }
 
     private View.OnClickListener nfcWriteOnclickListener = new View.OnClickListener(){
         @Override
         public void onClick(View v) {
-            
+
+            if(nfcTag==null){
+                Toast.makeText(getApplicationContext(),"NFC Tag Not detected",Toast.LENGTH_SHORT).show();
+            }else{
+                writeToNFC(nfcText.getText().toString());
+                Toast.makeText(getApplicationContext(),"Write Completed!",Toast.LENGTH_SHORT).show();
+            }
+
         }
+
     };
+
+    private void writeToNFC(String payload){
+
+    }
 }
--- FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/NFCStudentActivity.java
+++ FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/NFCStudentActivity.java
@@ -1,6 +1,8 @@
 package com.se17.attendancesystem;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -16,16 +18,43 @@ import android.widget.Toast;
 
 public class NFCStudentActivity extends AppCompatActivity {
 
+    private NfcAdapter nfcAdapter;
+    private PendingIntent pendingIntent;
+    private IntentFilter[] filterArray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfcstudent);
+        nfcAdapter =  NfcAdapter.getDefaultAdapter(this);
+        pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        IntentFilter filter = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            filter.addDataType("*/*");    /* Handles all MIME based dispatches.
+                                       You should specify only the ones that you need. */
+        }
+        catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+        filterArray = new IntentFilter[] {filter, };
     }
 
     /*
         onNewIntent will be called when the NDEF message is received by the mobile from an NFC Tag.
         Necessary registrations are made in the Manifest file.
      */
+
+    public void onPause() {
+        super.onPause();
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    public void onResume() {
+        super.onResume();
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, filterArray, null);
+    }
 
     @Override
     protected void onNewIntent(Intent intent) {
