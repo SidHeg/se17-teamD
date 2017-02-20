cb3ce7b85c0ad773679fe3759f1a7ee80787ed47
saravanan19 <bsaran19@gmail.com>
2017-02-19 23:28:56 -0500

    NFC Write layout created

--- FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/NFCProfessorActivity.java
+++ FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/NFCProfessorActivity.java
@@ -2,12 +2,33 @@ package com.se17.attendancesystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/*
+    Activity that helps professor to write payload in the NFC Tags
+ */
 
 public class NFCProfessorActivity extends AppCompatActivity {
+
+    private EditText nfcText;
+    private Button btnNfcWrite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfcprofessor);
+        nfcText = (EditText) findViewById(R.id.nfctext);
+        btnNfcWrite = (Button) findViewById(R.id.btnNFCWrite);
+
     }
+
+
+    private View.OnClickListener nfcWriteOnclickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            
+        }
+    };
 }
--- FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/NFCStudentActivity.java
+++ FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/NFCStudentActivity.java
@@ -9,6 +9,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+/*
+    Activity that handles the attendance registration of students using NFC Technology
+ */
+
+
 public class NFCStudentActivity extends AppCompatActivity {
 
     @Override
@@ -16,6 +21,11 @@ public class NFCStudentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfcstudent);
     }
+
+    /*
+        onNewIntent will be called when the NDEF message is received by the mobile from an NFC Tag.
+        Necessary registrations are made in the Manifest file.
+     */
 
     @Override
     protected void onNewIntent(Intent intent) {
@@ -32,6 +42,12 @@ public class NFCStudentActivity extends AppCompatActivity {
                 NdefRecord rec = messages[0].getRecords()[0];
                 String str = new String(rec.getPayload()).substring(3);
                 Toast.makeText(getApplicationContext(),str,Toast.LENGTH_LONG).show();
+
+                /*
+                    Todo:
+                        Authenticate the payload with backend and notify the student if he/she registered the
+                    attendance successfully
+                 */
 
             }
         }
