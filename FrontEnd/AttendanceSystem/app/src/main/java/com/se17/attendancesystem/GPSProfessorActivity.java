7ff49f4027b7742e44f49db3dc7f314414a57d07
saravanan19 <bsaran19@gmail.com>
2017-02-21 03:05:55 -0500

    GPS code development layout setup

--- FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/GPSProfessorActivity.java
+++ FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/GPSProfessorActivity.java
@@ -2,12 +2,26 @@ package com.se17.attendancesystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GPSProfessorActivity extends AppCompatActivity {
+
+    private Button btnGPS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpsprofessor);
+        btnGPS = (Button) findViewById(R.id.btnGPSProf);
+        btnGPS.setOnClickListener(btnGPSListener);
     }
+
+    private View.OnClickListener btnGPSListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+
+        }
+    };
 }
--- FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/GPSStudentActivity.java
+++ FrontEnd/AttendanceSystem/app/src/main/java/com/se17/attendancesystem/GPSStudentActivity.java
@@ -2,12 +2,27 @@ package com.se17.attendancesystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GPSStudentActivity extends AppCompatActivity {
+
+    private Button btnGPS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpsstudent);
+        btnGPS = (Button) findViewById(R.id.btnGPSStud);
+        btnGPS.setOnClickListener(btnGPSListener);
     }
+
+
+    private View.OnClickListener btnGPSListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+
+        }
+    };
 }
